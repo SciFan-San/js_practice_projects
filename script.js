7af51dd5764da357b87c1d33b47414deb084cb6b
SciFan-San <MDJohnston_EFC@protonmail.com>
2023-05-20 20:08:00 +0200

    updated & working

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
+const selectModal = document.querySelector(".modal-container").classList;
+
 //Creating Modal Class Prototype
 class Modal {
   //Prototype function: Defines variables & parameters for methods and new Modal classes
-  constructor() {
-
+  constructor(selectModal) {
+    this.selectModal = selectModal;
   };
 
   //Function: Sets modal visible
-  openModal() {
-
+  openModal(selectModal) {
+    this.selectModal.remove("modal-hid");
   };
 
   //Function: Sets modal hidden
-  closeModal() {
-
+  closeModal(selectModal) {
+    this.selectModal.add("modal-hid");
   };
 };
 
-let myModal = new Modal ();
+const myModal = new Modal (selectModal);
+
+let openButton = document.querySelector("button.open").addEventListener("click", function() {
+  myModal.openModal(selectModal)});
+let closeButton = document.querySelector("button.close").addEventListener("click", function() {
+  myModal.closeModal(selectModal)});
